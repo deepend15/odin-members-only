a24c53274b717c8a238b1a46aeed4ca8ff2e7e2a
deepend15 <170750487+deepend15@users.noreply.github.com>
2026-03-12 17:01:42 -0400

    Add code to populate postgres db

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,15 +1,24 @@
 import { Client } from "pg";
 
-// const SQL = `
-// CREATE TABLE IF NOT EXISTS [tableName] (
-//   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-//    [column info]
-// );
+const SQL = `
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR ( 255 ),
+  last_name VARCHAR ( 255 ),
+  username VARCHAR ( 255 ),
+  password VARCHAR ( 255 ),
+  member BOOLEAN DEFAULT FALSE,
+  admin BOOLEAN DEFAULT FALSE
+);
 
-// INSERT INTO [tableName] ([columns])
-// VALUES
-//   ([value info])
-// `;
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  title VARCHAR ( 50 ),
+  message TEXT,
+  time TIMESTAMP 
+)
+`;
 
 async function main() {
   console.log("Seeding...");
